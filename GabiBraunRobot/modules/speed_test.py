79ced95885f8d8502fbf5b4a71972ceba705a942
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 18:16:38 +0530

    Update speed_test.py

--- GabiBraunRobot/modules/speed_test.py
+++ GabiBraunRobot/modules/speed_test.py
@@ -1,7 +1,7 @@
 import speedtest
-from SaitamaRobot import DEV_USERS, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus
+from GabiBraunRobot import DEV_USERS, dispatcher
+from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
+from GabiBraunRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
                       Update)
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
