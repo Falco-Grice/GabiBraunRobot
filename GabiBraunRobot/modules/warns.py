abee4eaa7e8f5105a03ad06542cd779dec5162da
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 18:35:08 +0530

    Update warns.py

--- GabiBraunRobot/modules/warns.py
+++ GabiBraunRobot/modules/warns.py
@@ -3,21 +3,21 @@ import re
 from typing import Optional
 
 import telegram
-from SaitamaRobot import TIGERS, WOLVES, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (bot_admin,
+from GabiBraunRobot import TIGERS, WOLVES, dispatcher
+from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
+from GabiBraunRobot.modules.helper_funcs.chat_status import (bot_admin,
                                                            can_restrict,
                                                            is_user_admin,
                                                            user_admin,
                                                            user_admin_no_reply)
-from SaitamaRobot.modules.helper_funcs.extraction import (extract_text,
+from GabiBraunRobot.modules.helper_funcs.extraction import (extract_text,
                                                           extract_user,
                                                           extract_user_and_text)
-from SaitamaRobot.modules.helper_funcs.filters import CustomFilters
-from SaitamaRobot.modules.helper_funcs.misc import split_message
-from SaitamaRobot.modules.helper_funcs.string_handling import split_quotes
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.sql import warns_sql as sql
+from GabiBraunRobot.modules.helper_funcs.filters import CustomFilters
+from GabiBraunRobot.modules.helper_funcs.misc import split_message
+from GabiBraunRobot.modules.helper_funcs.string_handling import split_quotes
+from GabiBraunRobot.modules.log_channel import loggable
+from GabiBraunRobot.modules.sql import warns_sql as sql
 from telegram import (CallbackQuery, Chat, InlineKeyboardButton,
                       InlineKeyboardMarkup, Message, ParseMode, Update, User)
 from telegram.error import BadRequest
