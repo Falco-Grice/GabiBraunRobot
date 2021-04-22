c60e810607a526137829579c1a51f96966b4cb1a
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 18:37:27 +0530

    Update wiki.py

--- GabiBraunRobot/modules/wiki.py
+++ GabiBraunRobot/modules/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from GabiBraunRobot import dispatcher
+from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
