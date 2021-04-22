364f6de17d283b997c5214e4e27dc6f69dfd23e2
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 21:02:50 +0530

    Update __main__.py

--- GabiBraunRobot/__main__.py
+++ GabiBraunRobot/__main__.py
@@ -191,8 +191,8 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_photo(
-                SAITAMA_IMG,
-                PM_START_TEXT.format(
+               
+                 PM_START_TEXT.format(
                     escape_markdown(first_name),
                     escape_markdown(context.bot.first_name)),
                 parse_mode=ParseMode.MARKDOWN,
