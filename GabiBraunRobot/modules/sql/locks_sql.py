54b2eca5097b3542386181a4b679f9ccc4113430
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 13:42:13 +0530

    Update locks_sql.py

--- GabiBraunRobot/modules/sql/locks_sql.py
+++ GabiBraunRobot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from SaitamaRobot.modules.sql import SESSION, BASE
+from GabiBraunRobot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
