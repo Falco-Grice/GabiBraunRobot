6ce65c6dfd3fab4e6d22451eb3e86b12361af794
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 13:50:11 +0530

    Update users_sql.py

--- GabiBraunRobot/modules/sql/users_sql.py
+++ GabiBraunRobot/modules/sql/users_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.sql import BASE, SESSION
+from GabiBraunRobot import dispatcher
+from GabiBraunRobot.modules.sql import BASE, SESSION
 from sqlalchemy import (Column, ForeignKey, Integer, String, UnicodeText,
                         UniqueConstraint, func)
 
