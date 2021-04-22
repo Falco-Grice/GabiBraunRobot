0f2daddb4b5648f6f08f69f4ee5ab12f8a3c59de
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 17:18:03 +0530

    Update math.py

--- GabiBraunRobot/modules/math.py
+++ GabiBraunRobot/modules/math.py
@@ -1,8 +1,8 @@
 import math
 
 import pynewtonmath as newton
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from GabiBraunRobot import dispatcher
+from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
