d6f281906651bf77a373b6d33bc4225669f9ef3e
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 17:10:50 +0530

    Update get_common_chats.py

--- GabiBraunRobot/modules/get_common_chats.py
+++ GabiBraunRobot/modules/get_common_chats.py
@@ -1,9 +1,9 @@
 import os
 from time import sleep
 
-from SaitamaRobot import OWNER_ID, dispatcher
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user
-from SaitamaRobot.modules.sql.users_sql import get_user_com_chats
+from GabiBraunRobot import OWNER_ID, dispatcher
+from GabiBraunRobot.modules.helper_funcs.extraction import extract_user
+from GabiBraunRobot.modules.sql.users_sql import get_user_com_chats
 from telegram import Update
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, Filters
