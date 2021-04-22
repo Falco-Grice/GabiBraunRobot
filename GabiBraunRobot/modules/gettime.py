f415bd1575d576179bb95b649c113802bc6d714f
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 17:11:40 +0530

    Update gettime.py

--- GabiBraunRobot/modules/gettime.py
+++ GabiBraunRobot/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from SaitamaRobot import TIME_API_KEY, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from GabiBraunRobot import TIME_API_KEY, dispatcher
+from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
