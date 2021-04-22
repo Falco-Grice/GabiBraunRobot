fedc07965057c12b0bf44111c2ba32172b8e181a
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 17:25:26 +0530

    Update paste.py

--- GabiBraunRobot/modules/paste.py
+++ GabiBraunRobot/modules/paste.py
@@ -1,6 +1,6 @@
 import requests
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from GabiBraunRobot import dispatcher
+from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
