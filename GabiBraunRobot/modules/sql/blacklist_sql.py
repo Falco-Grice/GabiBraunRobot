e4421d7146f82902a7140b0ffab6a206ed63ba01
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 13:28:06 +0530

    Update blacklist_sql.py

--- GabiBraunRobot/modules/sql/blacklist_sql.py
+++ GabiBraunRobot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from SaitamaRobot.modules.sql import SESSION, BASE
+from GabiBraunRobot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
