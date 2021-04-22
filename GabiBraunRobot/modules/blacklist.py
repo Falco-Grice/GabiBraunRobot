9febfb9cb29f7dbd1e428ad61655f6b39ab98117
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 16:07:13 +0530

    Update blacklist.py

--- GabiBraunRobot/modules/blacklist.py
+++ GabiBraunRobot/modules/blacklist.py
@@ -6,18 +6,18 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-import SaitamaRobot.modules.sql.blacklist_sql as sql
-from SaitamaRobot import dispatcher, LOGGER
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from SaitamaRobot.modules.helper_funcs.extraction import extract_text
-from SaitamaRobot.modules.helper_funcs.misc import split_message
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.warns import warn
-from SaitamaRobot.modules.helper_funcs.string_handling import extract_time
-from SaitamaRobot.modules.connection import connected
+import GabiBraunRobot.modules.sql.blacklist_sql as sql
+from GabiBraunRobot import dispatcher, LOGGER
+from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
+from GabiBraunRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from GabiBraunRobot.modules.helper_funcs.extraction import extract_text
+from GabiBraunRobot.modules.helper_funcs.misc import split_message
+from GabiBraunRobot.modules.log_channel import loggable
+from GabiBraunRobot.modules.warns import warn
+from GabiBraunRobot.modules.helper_funcs.string_handling import extract_time
+from GabiBraunRobot.modules.connection import connected
 
-from SaitamaRobot.modules.helper_funcs.alternate import send_message, typing_action
+from GabiBraunRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 BLACKLIST_GROUP = 11
 
