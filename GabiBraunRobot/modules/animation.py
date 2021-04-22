43475515edd757af5271bd3bafc3442ec0cddc31
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 15:43:06 +0530

    Update animation.py

--- GabiBraunRobot/modules/animation.py
+++ GabiBraunRobot/modules/animation.py
@@ -4,9 +4,9 @@ from typing import List
 from telegram import Update
 from telegram.ext import run_async,CallbackContext
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
+from GabiBraunRobot import dispatcher
+from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
+from GabiBraunRobot.modules.helper_funcs.chat_status import user_admin
 
 #sleep how many times after each edit in 'love' 
 EDIT_SLEEP = 1
