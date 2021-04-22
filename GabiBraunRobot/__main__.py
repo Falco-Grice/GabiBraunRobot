4f7e53271e67f3233176b2a7fa3822f6c9c5fe03
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 23:31:40 +0530

    Update __main__.py

--- GabiBraunRobot/__main__.py
+++ GabiBraunRobot/__main__.py
@@ -54,9 +54,16 @@ def get_readable_time(seconds: int) -> str:
 PM_START_TEXT = """
 Hi {}, my name is {}! 
 I am an Anime themed group management bot.
-Build by @The Ghost Hunter for weebs, I specialize in managing anime and similar themed groups.
+Build by weebs for weebs, I specialize in managing anime and similar themed groups.
 You can find my list of available commands with /help.
 """
+PM_START_TEXT = """
+Hi {}, my name is {}! 
+I am an Anime themed group management bot.
+Build by weebs for weebs, I specialize in managing anime and similar themed groups.
+You can find my list of available commands with /help.
+"""
+
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
