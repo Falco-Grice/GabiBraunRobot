1dcb352bfb3509fde666a18a217be3b8871814ef
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 17:01:32 +0530

    Update disasters.py

--- GabiBraunRobot/modules/disasters.py
+++ GabiBraunRobot/modules/disasters.py
@@ -3,12 +3,12 @@ import json
 import os
 from typing import Optional
 
-from SaitamaRobot import (DEV_USERS, OWNER_ID, DRAGONS, SUPPORT_CHAT, DEMONS,
+from GabiBraunRobot import (DEV_USERS, OWNER_ID, DRAGONS, SUPPORT_CHAT, DEMONS,
                           TIGERS, WOLVES, dispatcher)
-from SaitamaRobot.modules.helper_funcs.chat_status import (dev_plus, sudo_plus,
+from GabiBraunRobot.modules.helper_funcs.chat_status import (dev_plus, sudo_plus,
                                                            whitelist_plus)
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user
-from SaitamaRobot.modules.log_channel import gloggable
+from GabiBraunRobot.modules.helper_funcs.extraction import extract_user
+from GabiBraunRobot.modules.log_channel import gloggable
 from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
