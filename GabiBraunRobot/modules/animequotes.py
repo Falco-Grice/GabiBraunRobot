a4ff2adabb443717336ebc4b4c3e097960ac6052
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 15:47:12 +0530

    Update animequotes.py

--- GabiBraunRobot/modules/animequotes.py
+++ GabiBraunRobot/modules/animequotes.py
@@ -6,11 +6,11 @@ from telegram import ParseMode, Update, ChatPermissions
 from telegram.ext import CallbackContext, run_async
 from telegram.error import BadRequest
 
-import SaitamaRobot.modules.animequotes_strings as animequotes_strings
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (is_user_admin)
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user
+import GabiBraunRobot.modules.animequotes_strings as animequotes_strings
+from GabiBraunRobot import dispatcher
+from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
+from GabiBraunRobot.modules.helper_funcs.chat_status import (is_user_admin)
+from GabiBraunRobot.modules.helper_funcs.extraction import extract_user
 
 @run_async
 def animequotes(update: Update, context: CallbackContext):
