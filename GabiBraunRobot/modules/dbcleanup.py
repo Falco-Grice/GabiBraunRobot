53088ee2e0a64f6d5bbf861fb45e034f62c5e89a
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 16:56:10 +0530

    Update dbcleanup.py

--- GabiBraunRobot/modules/dbcleanup.py
+++ GabiBraunRobot/modules/dbcleanup.py
@@ -1,9 +1,9 @@
 from time import sleep
 
-import SaitamaRobot.modules.sql.global_bans_sql as gban_sql
-import SaitamaRobot.modules.sql.users_sql as user_sql
-from SaitamaRobot import DEV_USERS, OWNER_ID, dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus
+import GabiBraunRobot.modules.sql.global_bans_sql as gban_sql
+import GabiBraunRobot.modules.sql.users_sql as user_sql
+from GabiBraunRobot import DEV_USERS, OWNER_ID, dispatcher
+from GabiBraunRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (CallbackContext, CallbackQueryHandler, CommandHandler,
