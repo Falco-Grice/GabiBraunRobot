0d30c4a88396b9740b8605961d213d3de3bd37a0
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 16:15:29 +0530

    Update cleaner.py

--- GabiBraunRobot/modules/cleaner.py
+++ GabiBraunRobot/modules/cleaner.py
@@ -1,11 +1,11 @@
 import html
 
-from SaitamaRobot import ALLOW_EXCL, CustomCommandHandler, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (bot_can_delete,
+from GabiBraunRobot import ALLOW_EXCL, CustomCommandHandler, dispatcher
+from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
+from GabiBraunRobot.modules.helper_funcs.chat_status import (bot_can_delete,
                                                            connection_status,
                                                            dev_plus, user_admin)
-from SaitamaRobot.modules.sql import cleaner_sql as sql
+from GabiBraunRobot.modules.sql import cleaner_sql as sql
 from telegram import ParseMode, Update
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
                           MessageHandler, run_async)
