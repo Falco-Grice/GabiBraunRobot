4c35de7fd573c258afaab7f85f0644540a878f34
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-22 17:26:13 +0530

    Update ping.py

--- GabiBraunRobot/modules/ping.py
+++ GabiBraunRobot/modules/ping.py
@@ -5,9 +5,9 @@ import requests
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-from SaitamaRobot import StartTime, dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import sudo_plus
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from GabiBraunRobot import StartTime, dispatcher
+from GabiBraunRobot.modules.helper_funcs.chat_status import sudo_plus
+from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
 
 sites_list = {
     "Telegram": "https://api.telegram.org",
