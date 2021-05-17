4647fd9700401649f4a17dd674d03b52ce64ba34
Falco-Grice <79639455+Falco-Grice@users.noreply.github.com>
2021-05-17 16:48:53 +0530

    Update __init__.py

--- GabiBraunRobot/__init__.py
+++ GabiBraunRobot/__init__.py
@@ -199,6 +199,7 @@ else:
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("saitama", API_ID, API_HASH)
 pgram = Client("SenseiPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+pbot = Client("SenseiPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
