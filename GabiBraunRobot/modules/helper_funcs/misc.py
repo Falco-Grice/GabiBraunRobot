b86f168ff58b38de8601c01023b6c1d9466c2658
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-05-23 14:06:30 +0530

    Update misc.py

--- GabiBraunRobot/modules/helper_funcs/misc.py
+++ GabiBraunRobot/modules/helper_funcs/misc.py
@@ -67,11 +67,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("🖕", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Back", callback_data="asuna_back"),
+                EqInlineKeyboardButton("Back", callback_data="gabi_back"),
              EqInlineKeyboardButton("👇", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="asuna_back")]]
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="gabi_back")]]
 
     return pairs
 
